8a0129c9fd1eae7e8aba432498a54be2015711c6
rlaxogh76 <btm.email2769@gmail.com>
2025-05-24 23:59:30 +0900

    NaverMap 이동 라우터 구현

--- src/Test.tsx
+++ src/Test.tsx
@@ -18,6 +18,20 @@ const Kakao_Map_Btn = styled.button`
   }
 `;
 
+const Naver_Map_Btn = styled.button`
+  background-color: #2fa2ff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  margin: 10px;
+  &:hover {
+    background-color: #217bfa;
+  }
+`;
+
 export default function Test() {
   const navigate = useNavigate();
 
@@ -25,10 +39,15 @@ export default function Test() {
     navigate("/projects/KakaoMap/main"); // Main.tsx 라우트로 이동
   };
 
+  const handleNaver = (): void => {
+    navigate("/projects/NaverMap/main");
+  }
+
   return (
     <div>
       <h1>Test</h1>
       <Kakao_Map_Btn onClick={handleClick}>카카오맵 이동</Kakao_Map_Btn>
+      <Naver_Map_Btn onClick={handleNaver}>네이버맵 이동</Naver_Map_Btn>
     </div>
   );
 }
